91825c5e02d9ff418b2ed82d600f9ca9db434316
human-netizen <niloyokasd@gmail.com>
2023-11-16 06:08:27 +0600

    bokaiqba

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.patch('/niloy/unsolvedProblem', authenticateToken, async (req, res) => {
         res.status(200).json({ user });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: 'Internal Server Error ok' });
     }
 });
 
